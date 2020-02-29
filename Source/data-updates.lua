7dfb5e9a074e3854bdc3afd606b019e46cb4c1cf
Leighzer <bwc_nothgiel@live.com>
2020-02-29 18:58:42 -0500

    v0.2.0

--- Source/data-updates.lua
+++ Source/data-updates.lua
@@ -3,25 +3,31 @@ function CreateLiquidScienceRecipeVanilla(scienceName, sortOrder, isEnabled)
     data:extend({        
         {
         type = "recipe",
-        name = "liquid-" .. scienceName,
-        energy_required = data.raw["recipe"][scienceName .. "-pack"].energy_required, 
+        name = "liquid-" .. scienceName .. "-science",
+        energy_required = data.raw["recipe"][scienceName .. "-science-pack"].energy_required, 
         enabled = isEnabled,
         category = "chemistry",
         ingredients = {},
-        results = {{type="fluid",name="liquid-" .. scienceName,amount=10}},        
+        results = {{type="fluid",name="liquid-" .. scienceName .. "-science",amount=10}},        
         subgroup = "leighzersciencebottling-vanilla-liquid-science",
-        order = sortOrder
+        order = sortOrder,
+        crafting_machine_tint = {
+                                primary = leighzermods.leighzersciencebottling.tints[scienceName],
+                                secondary = leighzermods.leighzersciencebottling.tints[scienceName],
+                                tertiary = leighzermods.leighzersciencebottling.tints[scienceName],
+                                quaternary = leighzermods.leighzersciencebottling.tints[scienceName],
+                                }
         }
     })
-    data.raw["recipe"]["liquid-" .. scienceName].ingredients = util.table.deepcopy(data.raw["recipe"][scienceName .. "-pack"].ingredients) --get normal ingredients
-    table.insert(data.raw["recipe"]["liquid-" .. scienceName].ingredients, {type="fluid",name="water",amount=50}) -- add water as ingredient
+    data.raw["recipe"]["liquid-" .. scienceName .. "-science"].ingredients = util.table.deepcopy(data.raw["recipe"][scienceName .. "-science-pack"].ingredients) --get normal ingredients
+    table.insert(data.raw["recipe"]["liquid-" .. scienceName .. "-science"].ingredients, {type="fluid",name="water",amount=50}) -- add water as ingredient
     if leighzermods.leighzersciencebottling.isPrecursoreEnabled then
-        table.insert(data.raw["recipe"]["liquid-" .. scienceName].ingredients, {type="fluid",name="liquid-precursore",amount=50}) --add precursore as ingredient
+        table.insert(data.raw["recipe"]["liquid-" .. scienceName .. "-science"].ingredients, {type="fluid",name="liquid-precursore",amount=50}) --add precursore as ingredient
     end
     if not isEnabled then --if not enabled
-        table.insert(data.raw["technology"][scienceName .. "-pack"].effects, {type = "unlock-recipe",recipe = "liquid-" .. scienceName})--add it as an unlock when pack is researched
+        table.insert(data.raw["technology"][scienceName .. "-science-pack"].effects, {type = "unlock-recipe",recipe = "liquid-" .. scienceName .. "-science"})--add it as an unlock when pack is researched
     end
-    table.insert(leighzermods.leighzersciencebottling.productivityEnabledRecipes,"liquid-" .. scienceName)
+    table.insert(leighzermods.leighzersciencebottling.productivityEnabledRecipes,"liquid-" .. scienceName .. "-science")
 end
 
 --science ore => science liquid
@@ -37,7 +43,13 @@ function CreateLiquidScienceRecipeScienceOre(scienceName,sortOrder,isEnabled)
         ingredients = {},
         results = {{type="fluid",name="liquid-" .. scienceName.."-science",amount=10}},        
         subgroup = "leighzersciencebottling-leighzerscienceores-liquid-science",
-        order = sortOrder
+        order = sortOrder,
+        crafting_machine_tint = {
+                                primary = leighzermods.leighzersciencebottling.tints[scienceName],
+                                secondary = leighzermods.leighzersciencebottling.tints[scienceName],
+                                tertiary = leighzermods.leighzersciencebottling.tints[scienceName],
+                                quaternary = leighzermods.leighzersciencebottling.tints[scienceName],
+                                }
         }
     })
     table.insert(data.raw["recipe"]["liquid-" .. scienceName.."-science-by-ore"].ingredients, {scienceName.."-science-ore",leighzermods.leighzerscienceores[scienceName.."Requirement"]})-- add science ore
@@ -53,12 +65,12 @@ end
 
 function VanillaLiquidScienceRecipes()--create vanilla item to liquid science recipes
     --create vanilla to liquid science recipes
-    CreateLiquidScienceRecipeVanilla("automation-science",0,true)
-    CreateLiquidScienceRecipeVanilla("logistic-science",1,false)
-    CreateLiquidScienceRecipeVanilla("military-science",2,false)
-    CreateLiquidScienceRecipeVanilla("chemical-science",3,false)
-    CreateLiquidScienceRecipeVanilla("production-science",4,false)
-    CreateLiquidScienceRecipeVanilla("utility-science",5,false)    
+    CreateLiquidScienceRecipeVanilla("automation",0,true)
+    CreateLiquidScienceRecipeVanilla("logistic",1,false)
+    CreateLiquidScienceRecipeVanilla("military",2,false)
+    CreateLiquidScienceRecipeVanilla("chemical",3,false)
+    CreateLiquidScienceRecipeVanilla("production",4,false)
+    CreateLiquidScienceRecipeVanilla("utility",5,false)    
 end
 
 function ScienceOreLiquidScienceRecipes()-- create science ores to liquid science recipes
@@ -87,11 +99,21 @@ function ScienceOreLiquidScienceRecipes()-- create science ores to liquid scienc
 end
 
 function MakeSciencePackRequireLiquid(scienceName)
-    local recipe = data.raw["recipe"][scienceName .. "-pack"]          
+    local recipe = data.raw["recipe"][scienceName .. "-science-pack"]          
     recipe.category = "chemistry"
-    recipe.ingredients = {{type="fluid",name="liquid-"..scienceName,amount=10},{"glass-bottle",1}}
-    recipe.result = nil    
-    recipe.results = {{scienceName .. "-pack",1}}    
+    recipe.ingredients = {{type="fluid",name="liquid-"..scienceName.."-science",amount=10},{"glass-bottle",1}}
+    local amnt = recipe.result_count
+    if not amnt then
+        amnt = 1
+    end
+    recipe.result = nil
+    recipe.results = {{scienceName .. "-science-pack",amnt}}
+    recipe.crafting_machine_tint = {
+                                    primary = leighzermods.leighzersciencebottling.tints[scienceName],
+                                    secondary = leighzermods.leighzersciencebottling.tints[scienceName],
+                                    tertiary = leighzermods.leighzersciencebottling.tints[scienceName],
+                                    quaternary = leighzermods.leighzersciencebottling.tints[scienceName],
+                                    }
 end
 
 function MakeSpaceSciencePackRequireLiquid()
@@ -103,7 +125,13 @@ function MakeSpaceSciencePackRequireLiquid()
     recipe.results = {{"space-science-pack",1}}  
     recipe.subgroup = "science-pack" 
     recipe.order = "z"    
-    recipe.hidden = false    
+    recipe.hidden = false
+    recipe.crafting_machine_tint = {
+                                    primary = leighzermods.leighzersciencebottling.tints[scienceName],
+                                    secondary = leighzermods.leighzersciencebottling.tints[scienceName],
+                                    tertiary = leighzermods.leighzersciencebottling.tints[scienceName],
+                                    quaternary = leighzermods.leighzersciencebottling.tints[scienceName],
+                                    }
 end
 
 function MoveSpaceScienceSubGroup()
@@ -112,12 +140,12 @@ function MoveSpaceScienceSubGroup()
 end
 
 function MakeSciencePacksRequireLiquid()
-    MakeSciencePackRequireLiquid("automation-science")
-    MakeSciencePackRequireLiquid("logistic-science")
-    MakeSciencePackRequireLiquid("military-science")
-    MakeSciencePackRequireLiquid("chemical-science")
-    MakeSciencePackRequireLiquid("production-science")
-    MakeSciencePackRequireLiquid("utility-science")
+    MakeSciencePackRequireLiquid("automation")
+    MakeSciencePackRequireLiquid("logistic")
+    MakeSciencePackRequireLiquid("military")
+    MakeSciencePackRequireLiquid("chemical")
+    MakeSciencePackRequireLiquid("production")
+    MakeSciencePackRequireLiquid("utility")
     if mods["leighzerscienceores"]then
         if leighzermods.leighzerscienceores.spaceEnabled then
             MakeSpaceSciencePackRequireLiquid()
@@ -126,39 +154,39 @@ function MakeSciencePacksRequireLiquid()
 end
 
 function HideScienceOreToPackRecipe(scienceName)
-    data.raw.recipe[scienceName.. "-ore-to-science"].hidden= true
+    data.raw.recipe[scienceName.. "-science-ore-to-science"].hidden= true
 end
 
 function HideScienceOreToPackRecipes()
     if leighzermods.leighzerscienceores.automationEnabled then
-        HideScienceOreToPackRecipe("automation-science")
+        HideScienceOreToPackRecipe("automation")
     end
     if leighzermods.leighzerscienceores.logisticEnabled then
-        HideScienceOreToPackRecipe("logistic-science")
+        HideScienceOreToPackRecipe("logistic")
     end
     if leighzermods.leighzerscienceores.militaryEnabled then
-        HideScienceOreToPackRecipe("military-science")
+        HideScienceOreToPackRecipe("military")
     end
     if leighzermods.leighzerscienceores.chemicalEnabled then
-        HideScienceOreToPackRecipe("chemical-science")
+        HideScienceOreToPackRecipe("chemical")
     end
     if leighzermods.leighzerscienceores.productionEnabled then
-        HideScienceOreToPackRecipe("production-science")
+        HideScienceOreToPackRecipe("production")
     end
     if leighzermods.leighzerscienceores.utilityEnabled then
-        HideScienceOreToPackRecipe("utility-science")
+        HideScienceOreToPackRecipe("utility")
     end        
     --if leighzermods.leighzerscienceores.spaceEnabled and not(mods["leighzerscienceores"] and leighzermods.leighzersciencebottling.onlyNeedToBottleScienceOres) then
-    --    HideScienceOreToPackRecipe("space-science")
+    --    HideScienceOreToPackRecipe("space")
     --end    
 end
 
 function AddLiquidToScienceRecipe(scienceName,isEnabled)
     local energy_required
     local order
-    if data.raw["recipe"][scienceName.. "-pack"] and data.raw["recipe"][scienceName.. "-pack"].energy_required then
-        energy_required = data.raw["recipe"][scienceName .. "-pack"].energy_required
-        order = data.raw["recipe"][scienceName .. "-pack"].order
+    if data.raw["recipe"][scienceName.. "-science-pack"] and data.raw["recipe"][scienceName.. "-science-pack"].energy_required then
+        energy_required = data.raw["recipe"][scienceName .. "-science-pack"].energy_required
+        order = data.raw["recipe"][scienceName .. "-science-pack"].order
     else
         energy_required = 50
         order = z
@@ -168,47 +196,53 @@ function AddLiquidToScienceRecipe(scienceName,isEnabled)
         {
             type = "recipe",
             enabled = isEnabled,
-            name = "alt-"..scienceName.."-pack",
+            name = "alt-"..scienceName.."-science-pack",
             energy_required = energy_required,
             category = "chemistry",
             subgroup = "leighzersciencebottling-alt-science-pack",
             order = order,
+            crafting_machine_tint = {
+                                    primary = leighzermods.leighzersciencebottling.tints[scienceName],
+                                    secondary = leighzermods.leighzersciencebottling.tints[scienceName],
+                                    tertiary = leighzermods.leighzersciencebottling.tints[scienceName],
+                                    quaternary = leighzermods.leighzersciencebottling.tints[scienceName],
+                                    },
             ingredients =
             {
-                {type="fluid",name="liquid-"..scienceName,amount=10},{"glass-bottle",1}
+                {type="fluid",name="liquid-"..scienceName.."-science",amount=10},{"glass-bottle",1}
             },
-            results = {{scienceName.."-pack",1}}
+            results = {{scienceName.."-science-pack",1}}
           },
     })
 
     if not isEnabled then --if not enabled
-        table.insert(data.raw["technology"][scienceName .. "-pack"].effects, {type = "unlock-recipe",recipe = "alt-"..scienceName.."-pack"})--add it as an unlock when pack is researched
+        table.insert(data.raw["technology"][scienceName .. "-science-pack"].effects, {type = "unlock-recipe",recipe = "alt-"..scienceName.."-science-pack"})--add it as an unlock when pack is researched
     end
-    table.insert(leighzermods.leighzersciencebottling.productivityEnabledRecipes,"alt-"..scienceName.."-pack")
+    table.insert(leighzermods.leighzersciencebottling.productivityEnabledRecipes,"alt-"..scienceName.."-science-pack")
 
 end
 
 function AddLiquidToScienceRecipes()
     if leighzermods.leighzerscienceores.automationEnabled then
-        AddLiquidToScienceRecipe("automation-science",true)
+        AddLiquidToScienceRecipe("automation",true)
     end
     if leighzermods.leighzerscienceores.logisticEnabled then
-        AddLiquidToScienceRecipe("logistic-science",false)
+        AddLiquidToScienceRecipe("logistic",false)
     end
     if leighzermods.leighzerscienceores.militaryEnabled then
-        AddLiquidToScienceRecipe("military-science",false)
+        AddLiquidToScienceRecipe("military",false)
     end
     if leighzermods.leighzerscienceores.chemicalEnabled then
-        AddLiquidToScienceRecipe("chemical-science",false)
+        AddLiquidToScienceRecipe("chemical",false)
     end
     if leighzermods.leighzerscienceores.productionEnabled then
-        AddLiquidToScienceRecipe("production-science",false)
+        AddLiquidToScienceRecipe("production",false)
     end
     if leighzermods.leighzerscienceores.utilityEnabled then
-        AddLiquidToScienceRecipe("utility-science",false)
+        AddLiquidToScienceRecipe("utility",false)
     end        
     --if leighzermods.leighzerscienceores.spaceEnabled then
-    --    AddLiquidToScienceRecipe("space-science",false)
+    --    AddLiquidToScienceRecipe("space",false)
     --end
 end
 
@@ -217,12 +251,16 @@ if mods["leighzerscienceores"] then
     ScienceOreLiquidScienceRecipes()--create science liquid from science ores
     if leighzermods.leighzersciencebottling.onlyNeedToBottleScienceOres then        
         AddLiquidToScienceRecipes()--adds duplicate science pack recipes that take liquid, so folks can liquify ores to make science or make science the good old fashioned way
-        MakeSpaceSciencePackRequireLiquid()
-        MoveSpaceScienceSubGroup()
+        if leighzermods.leighzerscienceores.spaceEnabled then
+            MakeSpaceSciencePackRequireLiquid()
+            MoveSpaceScienceSubGroup()
+        end
     else            
         VanillaLiquidScienceRecipes()--create science liquid from regular vanilla ingredients
         MakeSciencePacksRequireLiquid()--update science pack recipes to require bottling of liquid
-        MakeSpaceSciencePackRequireLiquid()        
+        if leighzermods.leighzerscienceores.spaceEnabled then
+            MakeSpaceSciencePackRequireLiquid()        
+        end
     end
     HideScienceOreToPackRecipes()
 else
@@ -230,49 +268,19 @@ else
     MakeSciencePacksRequireLiquid()--update science pack recipes to require bottling of liquid
 end
 
---if leighzerscienceores then
---
---    if only bottle science ores
---        dont touch regular science
---        create science ore to liquid
---        create liquid to science, in new category below normal science `2 sets of recipes to make science`
---    else
---        fuck up regular science
---        create regular ingredients to liquid
---        create science ore to liquid        
---        update science recipe to require liquid `1 set of recipes ot make science`
-
---else
---    create regular ingredients to liquid
---    update science recipe to require liquid `1 set of recipes ot make science`
-
---end
-
-
-
-function MoveBarrelRecipe(scienceName,sortOrder,isEnabled)
-    local item = data.raw["item"]["liquid-"..scienceName.."-barrel"]
+function MoveBarrelRecipe(scienceName,sortOrder)
+    local item = data.raw["item"]["liquid-"..scienceName.."-science-barrel"]
     item.subgroup = "leighzersciencebottling-liquid-science-barreling-fill"
     item.order = sortOrder
-    local fill = data.raw["recipe"]["fill-liquid-"..scienceName.."-barrel"]
+    local fill = data.raw["recipe"]["fill-liquid-"..scienceName.."-science-barrel"]
     fill.subgroup = "leighzersciencebottling-liquid-science-barreling-fill"
-    fill.order = sortOrder
-    --RemoveRecipeUnlock("fluid-handling",fill) --actually we are fine with leaving unlock effect under flui
-    local empty = data.raw["recipe"]["empty-liquid-"..scienceName.."-barrel"]
+    fill.order = sortOrder    
+    local empty = data.raw["recipe"]["empty-liquid-"..scienceName.."-science-barrel"]
     empty.subgroup = "leighzersciencebottling-liquid-science-barreling-empty"
-    empty.order = sortOrder
-    --RemoveRecipeUnlock("fluid-handling",empty)
-    --logic for either moving unlock effect of barrel recipe to somewhere else, or that if it should be unlocked straight away
-    --if not isEnabled then 
-    --    table.insert(data.raw["technology"][scienceName .. "-pack"].effects,{type="unlock-recipe",recipe=fill.name})--add barreling recipe to unlock when pack does
-    --    table.insert(data.raw["technology"][scienceName .. "-pack"].effects,{type="unlock-recipe",recipe=empty.name})
-    --else
-    --    fill.enabled = isEnabled -- enable the recipe
-    --    empty.enabled = isEnabled
-    --end
+    empty.order = sortOrder    
 end
 
-function MovePrecursoreBarrelRecipe(liquidName,sortOrder,isEnabled)
+function MovePrecursoreBarrelRecipe(liquidName,sortOrder)
     local item = data.raw["item"][liquidName.."-barrel"]
     item.subgroup = "leighzersciencebottling-liquid-science-barreling-fill"
     item.order = sortOrder
@@ -286,17 +294,17 @@ end
 
 function MoveBarrelRecipes()
     if leighzermods.leighzersciencebottling.isPrecursoreEnabled then
-        MovePrecursoreBarrelRecipe("liquid-precursore",0,true)
+        MovePrecursoreBarrelRecipe("liquid-precursore",0)
     end
-    MoveBarrelRecipe("automation-science",1,true)
-    MoveBarrelRecipe("logistic-science",2,false)
-    MoveBarrelRecipe("military-science",3,false)
-    MoveBarrelRecipe("chemical-science",4,false)
-    MoveBarrelRecipe("production-science",5,false)
-    MoveBarrelRecipe("utility-science",6,false)    
+    MoveBarrelRecipe("automation",1)
+    MoveBarrelRecipe("logistic",2)
+    MoveBarrelRecipe("military",3)
+    MoveBarrelRecipe("chemical",4)
+    MoveBarrelRecipe("production",5)
+    MoveBarrelRecipe("utility",6)    
     if mods["leighzerscienceores"] then
         if leighzermods.leighzerscienceores.spaceEnabled then
-            MoveBarrelRecipe("space-science",7,false)
+            MoveBarrelRecipe("space",7)
         end
     end
 end
--- Source/data.lua
+++ Source/data.lua
@@ -16,6 +16,16 @@ end
 
 if not leighzermods.leighzersciencebottling.tints then
     leighzermods.leighzersciencebottling.tints = {}
+
+    leighzermods.leighzersciencebottling.tints.automation = {r=219/255, g=50/255, b=50/255, a = 1} -- 219, 50, 50 these commented values are what I used for making the graphics mostly for my own reference
+    leighzermods.leighzersciencebottling.tints.logistic = {r=149/255, g=206/255, b=129/255, a = 1} --37.1, 64.1, 27.6
+    leighzermods.leighzersciencebottling.tints.military = {r=135/255, g=146/255, b=155/255, a = 1} --47.9, 51.3, 54.0
+    leighzermods.leighzersciencebottling.tints.chemical = {r=97/255, g=216/255, b=234/255, a = 1} --8.6,64.2,76.5
+    leighzermods.leighzersciencebottling.tints.production = {r=152/255, g=26/255, b=202/255, a = 1}--71.6,0.0,100
+    leighzermods.leighzersciencebottling.tints.utility = {r=208/255, g=177/255, b=20/255, a = 1}--71.6,60.9,6.9
+    leighzermods.leighzersciencebottling.tints.space = {r=222/255, g=222/255, b=222/255, a = 1}--57.3,57.3,57.3
+    --leighzermods.leighzersciencebottling.tints.advancedLogistic = {r=1,g=0.1,b=1, a=1}--100, 0, 66.7
+
     leighzermods.leighzersciencebottling.tints["precursore"] = {r=68/255, g=225/255, b=253/255, a = 1} 
     leighzermods.leighzersciencebottling.tints["sand"] = {r=230/255, g=224/255, b=194/255, a = 1} --70.7 63.0 28.5
 end
